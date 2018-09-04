6f63bf46ce1201b7b7306e5b22def97ea2384144
Rachel Inkala <rachelinkala@gmail.com>
2018-09-04 17:26:17 -0600

    added information to departments and items

---
+++ db/migrate/20180904213453_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.text :body
+      t.belongs_to :department, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
